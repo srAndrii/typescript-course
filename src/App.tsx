8c710895c99a13b740781602fdc315518c36f721
srAndrii <55akif5548@gmail.com>
2022-07-10 13:45:50 +0300

    Added Card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Card from "./component/card";
 
 const App = () => {
     return (
         <div>
-            Hi
+            <Card width='200px' height='200px' >
+                <button>Кнопка</button>
+
+                </Card>
         </div>
     );
 };
