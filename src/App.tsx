910f1d385aa7068767750ccbb68d20e21157499d
srAndrii <55akif5548@gmail.com>
2022-07-13 14:33:10 +0300

    Added UserItem component and fetched data from server

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
-import React from 'react';
-import Card from "./component/card";
+import React, { useState, useEffect} from 'react';
+import Card, {CardVariant} from "./component/Сard";
+import UserList from "./component/UserList";
+import {IUser} from "./types/types";
+import axios from "axios";
 
 const App = () => {
+    const [users, setUsers] = useState<IUser[]>([]);
+    useEffect(() => {
+
+        fetchUsers()
+    }, []);
+
+    async function fetchUsers(){
+        try{
+            const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users')
+            setUsers(response.data)
+        }catch (e){
+            alert(e)
+        }
+    }
+
     return (
         <div>
-            <Card width='200px' height='200px' >
+            <Card onClick={(num)=>console.log('click',num)} variant={CardVariant.outline} width='200px' height='200px' >
                 <button>Кнопка</button>
-
-                </Card>
+                <div>dfbfd</div>
+            </Card>
+            <UserList users={users}/>
         </div>
     );
 };
